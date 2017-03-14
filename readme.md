602db5bd956a44f07e3ec9447aadc6d237cdd76a
Paige Busch <93paigedf@gmail.com>
2017-03-13 21:34:58 -0600

    set up sql db

--- readme.md
+++ readme.md
@@ -1,12 +1,15 @@
 NEED:
-Title
 Logo
+Login
 Picture Slider
+Blog Photos
+Tour Photos
 Available Dates
 Book Now
 Payment Options
     Stripe(node)
 Node
     Create Blog Entries
+    Create Tours
 SQL
     Foreign Key
--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var massive = require("massive");
 
-var app = express();
 var port = 8080;
+
+var conn = massive.connectSync({
+  connectionString : "postgres://postgres:B710Ljr9v@@localhost/fitforadventures"
+});
+
+var app = module.exports = express();
+
+app.set('db', conn);
 
 app.use(express.static('../public'));
 app.use(bodyParser.json());
